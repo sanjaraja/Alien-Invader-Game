ca0e3d39836dc41489a7ce31193070bcd770404f
Sanjay Rajasekaran <rajasekaran.sanjay@gmail.com>
2019-06-22 19:04:57 -0700

    Added the run_game method which contains the main game engine

--- AlienGame/alien_invasion.py
+++ AlienGame/alien_invasion.py
@@ -1,2 +1,21 @@
 import pygame
-print("Hello")
+import sys 
+
+#Making method that will start main loop of game:
+def run_game():
+    #Creating a screen object:
+    pygame.init()
+    screen = pygame.display.set_mode((1100, 700))
+
+    #This is the loop that will be running the game:
+    while True:
+
+        #Looking for keyboard and mouse events in gameL:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        
+        #This will ensure that the drawing screen will be there:
+        pygame.display.flip()
+
+run_game()
