0999ffdf64663a90921dfd6d060bff030372bb54
Sanjay Rajasekaran <rajasekaran.sanjay@gmail.com>
2019-06-25 16:55:12 -0700

    Added refactoring on alien_invasion class through Settings class

--- AlienGame/alien_invasion.py
+++ AlienGame/alien_invasion.py
@@ -1,12 +1,14 @@
 import pygame
 import sys 
+from settings import Settings
 
 #Making method that will start main loop of game:
 def run_game():
     #Creating a screen object:
     pygame.init()
-    screen = pygame.display.set_mode((1100, 700))
-    game_color = (192, 192, 192)
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+
 
     #This is the loop that will be running the game:
     while True:
@@ -17,7 +19,7 @@ def run_game():
                 sys.exit()
 
         #Redrawing the screen after each pass through loop:
-        screen.fill(game_color)
+        screen.fill(ai_settings.game_color)
         
         #This will ensure that the drawing screen will be there:
         pygame.display.flip()
